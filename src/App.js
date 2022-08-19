8345bd53ec04a4e3b35f271d62e31c56d02abf34
Dela Ricch <DelaRicch@gmail.com>
2022-08-19 14:40:41 +0000

    Done setting up the environment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import useLocalStorage from 'use-local-storage';
+import './index.css';
+
+
+function App() {
+  return (
+    <section className="app">
+      
+    </section>
+  );
+}
+
+export default App;
